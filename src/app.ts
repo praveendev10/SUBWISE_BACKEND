12d35c57d7610de9e801035802f135ce276304c9
praveendev10 <praveenkumarnj10@gmail.com>
2026-08-25 01:36:47 +0530

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
-import authRouter from "./routes/auth.routes"; 
-import { Response,Request } from "express";
+import authRouter from "./routes/auth.routes";
+import { Response, Request } from "express";
+import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use("/auth", authRouter);
 app.get("/", (req: Request, res: Response) => {
